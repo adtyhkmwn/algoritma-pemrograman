70c8fc2d3e4fd91a28d4776fee16cc66f01fde47
aditya hikmawan <112555305+adtyhkmwn@users.noreply.github.com>
2025-04-19 16:21:27 +0700

    Add files via upload

---
+++ pertemuan 7/a&b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()	{
+	int a = 2;
+	int b = 6;
+	int c = 2;
+	
+	float d = 2;
+	float e = 5;
+	
+	int soal1 = (-2 + a + b) / c;
+	float soal2 = sqrt ((d / e) + pow (d , 2));
+	cout<<" hasil 1: "<<soal1<<endl;
+	cout<<" hasil 2: "<<soal2<<endl;
+	
+}
